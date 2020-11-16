d323355a31a77cd63409a12b3e2b0130cce76519
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-11-16 10:16:25 +0900

    heatdot generator

--- heatdot_gen.py
+++ heatdot_gen.py
@@ -28,37 +28,6 @@ if gpus:
 policy = mixed_precision.Policy('mixed_float16')
 mixed_precision.set_global_policy(policy)
 
-def gaussian_heatmap_batch(rr, cc, shape, sigma=10):
-    """
-    Returns a heat map of a point
-    Shape is expected to be (HEIGHT, WIDTH)
-    [r,c] should be the point i.e. opposite of pygame notation.
-
-    Parameters
-    ----------
-    rr : np.array
-        batch of row of the points
-    cc : np.array
-        batch of the points
-    shape : tuple of int
-        (HEIGHT, WIDTH)
-
-    Returns
-    -------
-    heatmap : np.array
-        shape : (HEIGHT, WIDTH)
-    """
-    coordinates = np.stack(np.meshgrid(
-        np.arange(shape[0],dtype=np.float32),
-        np.arange(shape[1],dtype=np.float32),
-        indexing='ij',
-    ), axis=-1)[np.newaxis,...]
-    keypoints = np.stack([rr,cc],axis=-1).reshape((-1,1,1,2))
-    heatmaps = np.exp(-(np.sum((coordinates-keypoints)**2,axis=-1))/(2*sigma**2))
-
-    return heatmaps
-
-
 vid_dir = Path(args.video)
 model_path = Path(args.load)
 bb_model = getattr(backbone_models, args.backbone)
@@ -73,16 +42,21 @@ model_wh = (320, 240)
 batch_size = int(args.batch)
 original_hw = (original_wh[1],original_wh[0])
 model_hw = (model_wh[1], model_wh[0])
+
+hw_ratio = np.divide(original_hw,model_hw)
+
 heatmap = np.zeros(original_hw, dtype=np.float)
+
 vid_names = os.listdir(vid_dir/'vids')
 vid_names.sort()
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 writer = cv2.VideoWriter(
-    str(vid_dir/'output'/'heatmap_vid.mp4'),
+    str(vid_dir/'output'/'heatdot_vid.mp4'),
     fourcc,
     30,
     original_wh,
 )
+
 for v_i in trange(len(vid_names)):
     vid_name = vid_names[v_i]
     original_frames = []
@@ -102,10 +76,10 @@ for v_i in trange(len(vid_names)):
         # Only nose
         nose_hms = output['nose']
         rr, cc = np.unravel_index(nose_hms.reshape((nose_hms.shape[0],-1)).argmax(axis=1),nose_hms.shape[1:])
-        nose_max_hms = gaussian_heatmap_batch(rr,cc, model_hw)
-        for nose_max_hm, original_frame in zip(nose_max_hms, original_frames[i*batch_size:(i+1)*batch_size]):
-            large_hm = cv2.resize(nose_max_hm, dsize=original_wh)
-            heatmap += large_hm
+        rr, cc = np.around(np.multiply([rr,cc],hw_ratio)).astype(np.int)
+        for j in range(batch_size):
+            original_frame = original_frames[i*batch_size+j]
+            heatmap[rr[j],cc[j]] += 1
             norm_hm = ((heatmap/(np.max(heatmap)))*255).astype(np.uint8)
             color_hm = cv2.applyColorMap(norm_hm, cv2.COLORMAP_JET)
             mixed = cv2.addWeighted(original_frame, 0.5, color_hm, 0.5, 0.0)
@@ -113,7 +87,7 @@ for v_i in trange(len(vid_names)):
     cap.release()
 final_norm_hm = ((heatmap/(np.max(heatmap)))*255).astype(np.uint8)
 colored_final_hm = cv2.applyColorMap(final_norm_hm, cv2.COLORMAP_JET)
-cv2.imwrite(str(vid_dir/'output'/'heatmap.png'), colored_final_hm)
+cv2.imwrite(str(vid_dir/'output'/'heatdot.png'), colored_final_hm)
 writer.release()
 
 
